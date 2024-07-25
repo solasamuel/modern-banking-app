3878cd210940af64cbfe5eddf315371525fd7cdf
Sola Samuel <solasamuel@Solas-MacBook-Pro.local>
2024-07-25 13:37:06 +0100

    added business and billingsections

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+  styles: string
+}
 
 const Button = (props: Props) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${props.styles}`}>Get Started</button>
   )
 }
 
